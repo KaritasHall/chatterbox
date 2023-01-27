220ce8466af814d1da7c200b15bf098151dca248
Karitas <karitashall@gmail.com>
2023-01-27 12:42:57 +0000

    cat facst endpoint

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { useState, useEffect } from "react";
 import Auth from "./auth";
 import Chat from "./Chat";
 
-
-
 function App() {
-//Setting a state for the login - default state is false (not logged in)
-const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //Setting a state for the login - default state is false (not logged in)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
       {/* A ternary operator that says is user logged in? if true then he goes to chat if not he goes to auth */}
-    {
-    isLoggedIn? <Chat></Chat> : 
-      <Auth login={setIsLoggedIn}></Auth>
-    }
+      {isLoggedIn ? <Chat></Chat> : <Auth login={setIsLoggedIn}></Auth>}
     </div>
   );
 }
